f22f47fdfd6f8d08186b8612474ba86ea8a4fef5
Grant Webb <dev@grantwebb.com>
2018-08-23 21:41:01 +0100

    Add project_service to project dependency

--- 01-override.tf
+++
@@ -1,8 +0,0 @@
-resource "google_project_service" "project_services" {
-  count = "${length(var.activate_apis)}"
-
-  project = "${local.project_id}"
-  service = "${element(var.activate_apis, count.index)}"
-
-  depends_on = ["google_project.project"]
-}
