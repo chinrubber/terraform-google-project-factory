05756d10d3fde67055bd3e6e8ecf8711d4e94924
Grant Webb <dev@grantwebb.com>
2018-08-24 11:49:58 +0100

    Changes

--- 02_override.tf
+++ 02_override.tf
@@ -27,7 +27,7 @@ resource "google_storage_bucket" "bucket_tf_state" {
 resource "google_service_account" "service_account_tf_admin" {
   account_id   = "${format("%s-%s", var.name, "tf")}"
   display_name = "${var.name} Project Service Account"
-  project      = "${local.project_id}"
+  project      = "${var.admin_project}"
 }
 
 /* A GCP IAM Entry on the Bucket permitting storage.admin access to the Service Account */
@@ -35,7 +35,7 @@ resource "google_service_account" "service_account_tf_admin" {
 resource "google_storage_bucket_iam_member" "bucket_iam_service_account_tf_admin" {
   bucket = "${google_storage_bucket.bucket_tf_state.name}"
   role   = "roles/storage.admin"
-  member = "${format("%s:%s@%s.iam.gserviceaccount.com", "serviceAccount", google_service_account.service_account_tf_admin.account_id, local.project_id)}"
+  member = "${format("%s:%s@%s.iam.gserviceaccount.com", "serviceAccount", google_service_account.service_account_tf_admin.account_id, var.admin_project)}"
 }
 
 /* A GCP IAM Entry on the Project permitting editor access to the Service Account */
@@ -43,5 +43,5 @@ resource "google_storage_bucket_iam_member" "bucket_iam_service_account_tf_admin
 resource "google_project_iam_member" "project_iam_service_account_tf_admin" {
   project = "${local.project_id}"
   role    = "roles/editor"
-  member  = "${format("%s:%s@%s.iam.gserviceaccount.com", "serviceAccount", google_service_account.service_account_tf_admin.account_id, local.project_id)}"
+  member  = "${format("%s:%s@%s.iam.gserviceaccount.com", "serviceAccount", google_service_account.service_account_tf_admin.account_id, var.admin_project)}"
 }
